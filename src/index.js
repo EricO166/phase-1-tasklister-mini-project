e66b61734ffb2db62d5f06968da42e30f60c3b1e
EricO166 <ochielsenor@gmail.com>
2025-07-11 10:36:48 +0300

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const tasksUl = document.getElementById("tasks");
+
+
+  form.addEventListener("submit", event => {
+    event.preventDefault();
+    const taskText = taskInput.value.trim();
+    if (taskText === "") return;
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "×";
+    deleteBtn.setAttribute("aria-label", `Delete ${taskText}`);
+    deleteBtn.addEventListener("click", () => li.remove());
+    li.append(" ", deleteBtn);
+
+
+    tasksUl.appendChild(li);
+    form.reset();
+  });
 });
